ea6b9793fd079065b2d94ba4816c33a1ff56872d
Iago Visie <iago.almeida@visie.com.br>
2021-09-13 19:19:18 -0300

    Remocao classe Telhado

--- main.py
+++ main.py
@@ -15,58 +15,6 @@ from textures.loader import TEXTURES
 
 window = Window('OpenGL Practice', 500, 500)
 
-class Telhado(Basic):
-    def draw(self, own_matrix):
-        if own_matrix:
-            glPushMatrix()
-
-        raio = 1.8
-        altura_base = 0.6
-        altura_pontas = 0.1
-        distancia_pilares = 0.6
-        
-        glTranslatef(self.position_x, self.position_y, self.position_z)
-        glColor(self.color)
-        glBegin(GL_TRIANGLE_FAN)
-
-        #Ponto central
-        glVertex3f(0.0, altura_base, 0.0)
-        glNormal3f(0, 0.7, 0)
-
-        #Pilares leste
-        glVertex3f(-raio, altura_pontas, +distancia_pilares)
-        glNormal3f(-0.9, 0.4, -0.9)
-        glVertex3f(-raio, altura_pontas, -distancia_pilares)
-        glNormal3f(-0.9, 0.4, 0.9)
-
-        #Pilares norte
-        glVertex3f(-distancia_pilares, altura_pontas, -raio)
-        glNormal3f(-0.9, 0.4, -0.9)
-        glVertex3f(+distancia_pilares, altura_pontas, -raio)
-        glNormal3f(0.9, 0.4, -0.9)
-
-        #Pilares oeste
-        glVertex3f(+raio, altura_pontas, -distancia_pilares)
-        glNormal3f(0.9, 0.4, -0.9)
-        glVertex3f(+raio, altura_pontas, +distancia_pilares)
-        glNormal3f(0.9, 0.4, 0.9)
-
-        #Pilares sul
-        glVertex3f(+distancia_pilares, altura_pontas, +raio)
-        glNormal3f(0.9, 0.4, -0.9)
-        glVertex3f(-distancia_pilares, altura_pontas, +raio)
-        glNormal3f(-0.9, 0.4, -0.9)
-
-        #Voltando para o inicio
-        glVertex3f(-raio, altura_pontas, +distancia_pilares)
-        glNormal3f(0,0,1)
-
-        glEnd()
-        # glutWireCone(self.size, self.height, 10, 10)
-
-        if own_matrix:
-            glPopMatrix()
-
 agua = Vertex(
     0.0, -0.8, 0.0,
     GL_POLYGON,
