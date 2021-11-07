10a3362f3d285b38ff92391c428537a9a8efedda
Augusto Cardoso Setti <augustocsetti@live.com>
2021-11-07 12:49:51 -0300

    limpando

--- classes/basic.py
+++ classes/basic.py
@@ -17,13 +17,7 @@ class Basic:
         self.type       = 'basic'
         self.lightning  = True
 
-    def setTexture(self, texture):#file_name):
-        #grass_image = Image.open(file_name).convert('RGBA')
-        # self.image = {
-        #     "height":   grass_image.height,
-        #     "width":    grass_image.width,
-        #     "data":     numpy.array(list(grass_image.getdata()), numpy.uint8)
-        # }
+    def setTexture(self, texture):
         self.image = texture
 
     def setOpacity(self, opacity):
@@ -44,24 +38,6 @@ class Basic:
         self.position_x += x
         self.position_y += y
         self.position_z += z
-
-    def colliding_x(self, x):
-        raio = 2
-        in_x = self.position_x < (x + self.size*raio) and self.position_x > (x - self.size*raio)
-        return self.type != 'cone' and in_x
-
-    def colliding_z(self, z):
-        raio = 2
-        in_z = self.position_z < (z + self.size*raio) and self.position_z > (z - self.size*raio)
-        return self.type != 'cone' and in_z
-
-    def colliding_y(self, y):
-        raio = 2
-        in_y = self.position_y < (y + self.size*raio) and self.position_y > (y - self.size*raio)
-        return self.type != 'cone' and in_y
-
-    def draw():
-        print('Oi')
 
     def animate(self):
         try:
@@ -95,7 +71,6 @@ class Basic:
                 self.animation['direction'] = "start" if direction == "end" else "end"
         except:
             return
-
 
     def setAnimation(self, speed, end_x, end_y, end_z, start_x=None, start_y=None, start_z=None):
         self.animation = {
